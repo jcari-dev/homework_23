548c339805840ba4f11c0d7b142357c8d0f64453
Jorge Caridad <jorgecaridad@Jorges-MacBook-Air.local>
2021-07-09 13:45:45 -0400

    Exercise No.1 Bonus Completed

--- prettybank.py
+++ prettybank.py
@@ -2,19 +2,34 @@ class BankAccount:
     def __init__(self, account_type, balance):
         self.type = account_type
         self.balance = balance
+        self.overdraft_fees = 0
 
     def deposit(self, amount):
         self.balance += amount
-        print(f"You deposited ${amount}. Your new balance is ${self.balance} ({self.type} account)")
+        print(
+            f"You deposited ${amount}. Your new balance is ${self.balance} ({self.type} account)")
         return self.balance
-    
+
     def withdraw(self, amount):
-        self.balance -= amount
-        print(f"You withdrawed ${amount}, Your new balance is ${self.balance} ({self.type} account)")
-        return self.balance
+        if(self.balance - amount <= -100):
+            print(f"Transaction Declined")
+        elif(self.balance - amount < 0):
+            amount += 20
+            self.balance -= amount
+            print(
+                f"This transaction was allowed (${amount-20}), but an overdraft fee of $20 was charged (${amount} total). Your current balance {self.balance}")
+        else:
+            self.balance -= amount
+            print(
+                f"You withdrawed ${amount}, Your new balance is ${self.balance} ({self.type} account)")
+            return self.balance
+
 
 BBVAsavings = BankAccount("savings", 500)
-BBVAcheckings = BankAccount("checking", 0)
+# BBVAcheckings = BankAccount("checking", 0)
 
-BBVAsavings.withdraw(300)
-BBVAcheckings.deposit(300)
+BBVAsavings.withdraw(500)  # Balance 0
+BBVAsavings.withdraw(20)  # 520+20 Balance -40
+BBVAsavings.withdraw(40)  # 40+40+20 Balance -100
+BBVAsavings.withdraw(20)  # Transaction Declined
+# BBVAcheckings.deposit(300)
